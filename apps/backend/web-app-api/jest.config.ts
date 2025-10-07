e9a9db142ac1fe6e77e9fc23e819c1a30f114719
ayushsanjdev <ayushsanjpro@gmail.com>
2025-10-08 00:04:37 +0530

    add: monorepo nx with repo

---
+++ apps/backend/web-app-api/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: '@my-fullstack-app/web-app-api',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: 'test-output/jest/coverage',
+};
